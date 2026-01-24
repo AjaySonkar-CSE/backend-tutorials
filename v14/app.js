43d0a3ed0c2f45a35ed242d4d25311331a828b97
AjaySonkar-CSE <ajaysonkar22044@gmail.com>
2026-01-24 17:08:51 +0530

    v14 done

--- v14/app.js
+++ v14/app.js
@@ -15,7 +15,7 @@ app.get('/password', function (req, res){
     });
 })
 
-app.get('/Pcheck', function (req, res){
+app.get('/passwordtest', function (req, res){
     bcrypt.compare("myPassword", "$2b$10$tSjkC28ea8Vbh3/eACQZKObTyGUJTDZYBNLYkcO4MS5z1CsVrYtyu", function(err, result) {
         console.log(result);
     });
@@ -25,16 +25,15 @@ app.get('/Pcheck', function (req, res){
 
 app.get('/jwtt', function (req, res){
     let token = jwt.sign({email: "aja@example.com"}, "secret");
-    console.log("token", token);
+    res.cookie("token", token);
     res.send("done");
 })
 
 app.get("/readjwt", function(req, res){
     let data = jwt.verify(req.cookies.token, "secret");
     console.log(data);
-    res.send("done");
 })
-
+//         cookies set and read
 app.get('/', function (req, res){
     res.cookie("name", "Raigarh SM");
     res.send("done");
