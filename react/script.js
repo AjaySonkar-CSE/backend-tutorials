35412c95bc929d463c3c762f1b43fbb64d6b5f84
AjaySonkar-CSE <ajaysonkar22044@gmail.com>
2026-01-30 18:33:40 +0530

    react 11:32

---
+++ react/script.js
@@ -0,0 +1,25 @@
+// DOM ek API hai jiske through hum JavaScript se HTML elements ko create, update, delete kar sakte hain.
+
+
+// react js ek javascript library hia jo user interface banane ke liye use hoti hia
+
+// ReactDOM React ko browser ke DOM me render karne ke liye use hota hai.(website React se banti hai, ReactDOM sirf render karta hai)
+
+//  React se web applications banti hain.
+//  React Native se mobile applications (Android/iOS) banti hain.
+
+// react js CDN
+
+// =======================================
+
+// var root = document.querySelector('#root')
+
+// var h1 = document.createElement('h1')
+
+// h1.innerHTML = "hello jjjjjjjjjjjj"
+
+// root.appendChild(h1)
+
+// =======================================
+
+console.log(React);
