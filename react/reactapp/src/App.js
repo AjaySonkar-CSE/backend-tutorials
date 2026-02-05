8a2ddba549392386ce5e9a0fa6240939dfeee8be
AjaySonkar-CSE <ajaysonkar22044@gmail.com>
2026-02-05 11:28:47 +0530

    2:12:00

--- react/reactapp/src/App.js
+++ react/reactapp/src/App.js
@@ -46,11 +46,17 @@
 
 import React from 'react';
 import Resturant from './component/Basics/Resturant';
+import UseState from './component/Hooks/useState';
+import UseEffect from './component/Hooks/useEffect';
+import UseReducer from './component/Hooks/useReducer';
 
 const App = () => {
   return (
     <div>
-      <Resturant />
+      {/* <Resturant /> */}
+      {/* <UseState /> */}
+      {/* <UseEffect /> */}
+      <UseReducer />
 
     </div>
   )
