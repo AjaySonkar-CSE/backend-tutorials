c3cd92a6ac3d58ced11fe250707918c1aa9edaa7
AjaySonkar-CSE <ajaysonkar22044@gmail.com>
2026-02-07 18:26:04 +0530

    2:38:00

--- react/reactapp/src/component/todoreact/todo.js
+++ react/reactapp/src/component/todoreact/todo.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.css";
 
 const Todo = () =>{
+    const [inputdata, setInputData] = useState("");
+    const [items, setItems] = useState([]);
+
+    // add items
+    const addItem = () => {
+        if(!inputdata){
+            alert("plz fill the data");
+        }else {
+            const myNewInputData = {
+                id: new Date().getTime().toString(),
+                name: inputdata,
+            }
+            setItems([... items, myNewInputData])
+            setInputData("");
+        }
+    }
+
+    //  delete items
+
+    const deleteItem = (index) =>{
+        const updateItem = items.filter((curElem) =>{
+            return curElem.id === index;
+        })
+    }
     return(
         <>
            <div className='main-div'>
@@ -14,10 +38,31 @@ const Todo = () =>{
                     <input type="text"
                     placeholder="write Here Add Item"
                     className="form-control"
+                    value={inputdata}
+                    onChange={(event) => setInputData(event.target.value)}
                     />
-                    <i className="fa fa-plus add-btn"></i>
+                    <i className="fa fa-plus add-btn" onClick={addItem}></i>
+                </div>
+                {/* show our items */}
+
+                <div className="showItems">
+                    {items.map((curElem) => {
+                        return(
+                            <div className="eachItem" key={curElem.id}>
+                                <h3>{curElem.name}</h3>
+                                <div className="todo-btn">
+                                    <i className="far fa-edit add-btn"></i>
+                                    <i className="far fa-trash-alt add-btn" onClick={() => deleteItem(curElem.id)}></i>
+                                </div>
+                            </div>
+
+                        )
+                    })}
+                    
+
                 </div>
 
+                {/* remove all */}
                 <div className="showItems">
                     <button className="btn effect04" data-sm-link-text="Remove All">
                         <span>CHECK LIST</span></button>
