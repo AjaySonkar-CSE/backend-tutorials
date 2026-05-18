f5070e5f8703a700709b58ad10754c00a57f0026
AjaySonkar-CSE <ajaysonkar22044@gmail.com>
2026-05-18 16:28:34 +0530

    v24 continue

--- mern_thapa/client/src/pages/Home.jsx
+++ mern_thapa/client/src/pages/Home.jsx
@@ -1,3 +1,53 @@
 export const Home = () => {
-    return <h1>Home Page    </h1>
+    return <>
+    
+    <main>
+        <selection className="section-hero">
+            <div className="container grid grid-two-column">
+                <div className="section-hero-data">
+                    <p className="hero-top-data">This is me</p>
+                    <h1 className="hero-heading">I am a MERN Stack Developer</h1>
+                    <p className="hero-para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ex.</p>
+                    <div className="btn btn-group">
+                        <a href="/contact">
+                        <button className="btn btn-primary">Contact Me</button>
+                        </a>
+                        <a href="/services">
+                        <button className="btn btn-secondary">learn More</button>
+                        </a>
+                    </div>
+                </div>
+
+                <div className="section-hero-image">
+                    <picture>
+                        <img src="./public/register.jpg" alt="hero image" className="hero-img" width="400" height=
+                        "400" />
+                    </picture>
+                </div>
+
+            </div>
+        </selection>
+
+        <section className="section-analysis">
+            <div className="container grid grid-four-column">
+                <div className="div1">
+                    <h3>10K</h3>
+                    <p>Happy Customers</p>  
+                </div>
+                <div className="div1">
+                    <h3>20K</h3>
+                    <p>Projects Completed</p>  
+                </div>
+                <div className="div1">
+                    <h3>30K</h3>
+                    <p>Happy Customers</p>  
+                </div>
+                <div className="div1">
+                    <h3>40K</h3>
+                    <p>Happy Customers</p>  
+                </div>
+            </div>
+        </section>
+    </main>
+    </>
 }
