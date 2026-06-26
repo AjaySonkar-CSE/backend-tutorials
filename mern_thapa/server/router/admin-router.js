58227a322a669a58f3043cc4814496654ac1e8a9
AjaySonkar-CSE <ajaysonkar22044@gmail.com>
2026-06-26 17:18:49 +0530

    v40 done

--- mern_thapa/server/router/admin-router.js
+++ mern_thapa/server/router/admin-router.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 const { getAllUsers } = require("../controllers/admin-controller");
+const { getAllContacts } = require("../controllers/admin-controller");
 
 router.route('/users').get(getAllUsers);
+router.route('/contacts').get(getAllContacts);
 
 
 module.exports = router;
