22d5390dd1be1b7f3f57a0366c2b15c6aea167f7
AjaySonkar-CSE <ajaysonkar22044@gmail.com>
2026-06-27 12:37:56 +0530

    v42 done

--- mern_thapa/server/router/admin-router.js
+++ mern_thapa/server/router/admin-router.js
@@ -6,12 +6,13 @@ const { getAllUsers } = require("../controllers/admin-controller");
 const { getAllContacts } = require("../controllers/admin-controller");
 
 const authMiddleware = require("../middlewares/auth-middleware");
+const adminMiddleware = require("../middlewares/admin-middleware");
 
 const router = express.Router();
 
 
-router.route('/users').get(authMiddleware, authMiddleware, getAllUsers);
-router.route('/contacts').get(authMiddleware, getAllContacts);
+router.route('/users').get(authMiddleware, adminMiddleware, getAllUsers);
+router.route('/contacts').get(authMiddleware, adminMiddleware, getAllContacts);
 
 
 module.exports = router;
